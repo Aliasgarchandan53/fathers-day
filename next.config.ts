9e3be78ae45a5b2474167daa6f968eabc408ce39
aliasgar-Healthsutra <ali.asgar@healthsutra.ai>
2025-06-15 19:51:03 +0530

    scripts edited

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // tells Next.js to use static export
+  images: {
+    unoptimized: true // required for next export
+  }
 };
 
 export default nextConfig;
